ae67f74fa2315ad0bff0befa8697153cf297f850
王水胜 <602466011@qq.com>
2019-09-06 09:24:48 +0800

    更新：全局挂载axios

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import request from './utils/request'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 全局挂载
+Vue.prototype.$http = request
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
