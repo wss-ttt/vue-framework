3929a68a3283e6c9eed6480cd8177a803b0ab51b
wangshuisheng <602466011@qq.com>
2020-02-26 13:49:09 +0800

    更新:测试后端设置跨域请求 和 前端配置跨域请求

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,24 +1,80 @@
 <template>
-    <div class="wrapper">
-        index
+  <div class="wrapper">
+    <div id="main"></div>
+    <div class="content">
+      <h1>测试发送请求</h1>
+      tips:该请求时后端配置了 允许跨域请求
+      <br>
+      <button type="button" @click="getData">发送请求获取数据</button>
+      <hr>
+      tip:该请求 在前端进行跨域请求配置
+      <br>
+      <button type="button" @click="getData2">发送请求获取数据</button>
     </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    components:{},
-    props:{},
-    data(){
-      return {
-          
-      }
+import echarts from 'echarts'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    // 测试图表初始
+    initChart() {
+      let myChart = echarts.init(document.getElementById('main'))
+      myChart.setOption({
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      })
     },
-    watch:{},
-    computed:{},
-    methods:{},
-    created(){},
-    mounted(){}
+    getData() {
+      // 测试发送请求
+      this.$http({
+        url:'http://localhost:3000/users/list',// 如果没有配置baseURL,这个地方就写完整的接口地址
+        method: 'get',
+      }).then((data)=>{
+        console.log(data);
+      })
+    },
+    getData2(){
+      this.$http({
+        url:'/api/users/list',
+        method: 'get',
+      }).then((data)=>{
+        console.log(data);
+      })
+    }
+  },
+  created() {},
+  mounted() {
+    this.initChart()
   }
+}
 </script>
-<style scoped>
+<style scoped lang="scss">
+.wrapper {
+  #main{
+    height: 240px;
+    background-color: antiquewhite;
+  }
+}
 </style>
