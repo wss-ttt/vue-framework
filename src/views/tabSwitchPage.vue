e8a6b9c1ce21a6a9c22f7cc3b68c5253c9b2d58d
wangshuisheng <602466011@qq.com>
2020-03-24 10:17:57 +0800

    添加:添加测试页面

---
+++ src/views/tabSwitchPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-switch">
+    <v-tab defaultKey="1" @on-click="changeTab">
+      <v-tab-pane actKey="1" label="全部">
+        <div>页面1</div>
+      </v-tab-pane>
+      <v-tab-pane actKey="2" label="推荐">
+        <div>页面2</div>
+      </v-tab-pane>
+      <v-tab-pane actKey="3" label="最新">
+        <div>页面3</div>
+        <v-test>好好学习 天天向上</v-test>
+      </v-tab-pane>
+    </v-tab>
+  </div>
+</template>
+
+<script>
+import VTest from '@/components/Test.vue'
+import VTab from '@/components/TabSwitch/Tabs.vue'
+import VTabPane from '@/components/TabSwitch/TabPane.vue'
+export default {
+  components: {
+    VTest,
+    VTab,
+    VTabPane
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    changeTab(item, index) {
+      //调数据
+      console.log(item, index)
+    }
+  },
+  filter() {},
+  computed: {},
+  watch: {}
+}
+</script>
+<style scoped>
+/* .tab-switch {
+  width: 500px;
+  height: 400px;
+  background-color: aliceblue;
+} */
+</style>
