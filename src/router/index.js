9f3a622dcaa96852efc0766a890672564dd0eb22
wangshuisheng <602466011@qq.com>
2020-03-25 11:01:43 +0800

    更新:路由

--- src/router/index.js
+++ src/router/index.js
@@ -12,14 +12,19 @@ export default new Router({
       component: HelloWorld
     },
     {
-      path:'/index',
-      name:'index',
-      component:resovle=>require(['@/views/index.vue'],resovle)
+      path: '/index',
+      name: 'index',
+      component: resovle => require(['@/views/index.vue'], resovle)
     },
     {
-      path:'/tabSwitchPage',  // 测试tab切换组件的测试页面
-      name:'tabSwitchPage',
-      component:resovle=>require(['@/views/001-tabSwitchPage.vue'],resovle)
+      path: '/tabSwitchPage',  // 测试tab切换组件的测试页面
+      name: 'tabSwitchPage',
+      component: resovle => require(['@/views/001-tabSwitchPage.vue'], resovle)
+    },
+    {
+      path: '/tablePage',  // 测试table组件的测试页面
+      name: 'tablePage',
+      component: resovle => require(['@/views/002-tablePage.vue'], resovle)
     }
   ]
 })
