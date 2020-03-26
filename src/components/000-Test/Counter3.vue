25e0b000472a14b53e576085ac9269779bb46b3e
wangshuisheng <602466011@qq.com>
2020-03-26 13:32:48 +0800

    添加:Counter3.vue组件

---
+++ src/components/000-Test/Counter3.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="counter">
+    <div class="counter_btn" @click="changeValue(-1)">-</div>
+    <div class="counter_val">{{currentVal}}</div>
+    <div class="counter_btn" @click="changeValue(1)">+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      currentVal: this.value
+    }
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    changeValue(val) {
+      this.currentVal += val * 1
+      this.$emit('on-change', this.currentVal)
+    },
+  },
+  filter() {},
+  computed: {
+  },
+  watch: {}
+}
+</script>
+<style scoped>
+.counter {
+  width: 100px;
+  text-align: center;
+}
+.counter_btn {
+  width: 100px;
+  height: 40;
+  line-height: 40px;
+  cursor: pointer;
+  background-color: aliceblue;
+}
+</style>
