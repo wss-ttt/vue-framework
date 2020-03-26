f6dfd01d2a0613d2ec78d0db1913c55014030fe1
wangshuisheng <602466011@qq.com>
2020-03-26 12:57:50 +0800

    添加:添加Counter.vue组件

---
+++ src/components/000-Test/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="counter">
+        <div class="counter_btn" @click="onMinus">-</div>
+        <div class="counter_val">{{value}}</div>
+        <div class="counter_btn" @click="onPlus">+</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    components:{},
+    props:{
+        value:{
+            type:Number,
+            default:0
+        },
+        onMinus:Function,  // 类型是一个函数
+        onPlus:Function,  //  类型是一个函数
+    },
+    data(){
+      return {
+          
+      }
+    },
+    created(){},
+    mounted(){},
+    activated(){},
+    deactivated(){},
+    updated(){},
+    destroyed(){},
+    methods:{},
+    filter(){},
+    computed:{},
+    watch:{},
+  }
+</script>
+<style scoped>
+.counter{
+  width: 100px;
+  text-align: center;
+}
+.counter_btn{
+  width: 100px;
+  height: 40;
+  line-height: 40px;
+  cursor: pointer;
+  background-color: aliceblue;
+}
+
+</style>
---
+++ src/components/000-Test/Counter2.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="counter">
+    <div class="counter_btn" @click="changeValue(-1)">-</div>
+    <div class="counter_val">{{value}}</div>
+    <div class="counter_btn" @click="changeValue(1)">+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      currentVal: this.value
+    }
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    changeValue(val) {
+      // val*1 -> 可以将字符串转换为Number类型
+      this.currentVal += val * 1
+      this.$emit('input', this.currentVal) // 定义input事件
+    },
+  },
+  filter() {},
+  computed: {
+  },
+  watch: {}
+}
+</script>
+<style scoped>
+.counter {
+  width: 100px;
+  text-align: center;
+}
+.counter_btn {
+  width: 100px;
+  height: 40;
+  line-height: 40px;
+  cursor: pointer;
+  background-color: aliceblue;
+}
+</style>
