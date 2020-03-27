e95a833539743a45cfcc6b7c4be069a14ec1ad41
wangshuisheng <602466011@qq.com>
2020-03-27 10:28:40 +0800

    添加:003-cardPage

---
+++ src/views/003-cardPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrapper">
+      <v-card class="card-box" shadow="hover">
+          <div slot="header">给我一首歌的时间</div>
+          <div v-for="i in 4" :key="i">好好学习</div>
+      </v-card> 
+   </div>
+</template>
+
+<script>
+import VCard from '@/components/003-card'
+  export default {
+    components:{
+        VCard
+    },
+    props:{},
+    data(){
+      return {
+          
+      }
+    },
+    created(){},
+    mounted(){},
+    activated(){},
+    deactivated(){},
+    updated(){},
+    destroyed(){},
+    methods:{},
+    filter(){},
+    computed:{},
+    watch:{},
+  }
+</script>
+<style scoped>
+.card-box{
+    width: 400px;
+}
+</style>
