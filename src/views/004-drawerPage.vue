0c1b49c0983834d5533026a313b4b3cac03f16e1
wangshuisheng <602466011@qq.com>
2020-03-30 21:56:22 +0800

    添加:drawerPage页面

---
+++ src/views/004-drawerPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <button @click="display = true">打开抽屉</button>
+    <v-drawer :display.sync="display" :inner="true" :width="drawerWidth" :mask="false"></v-drawer>
+  </div>
+</template>
+
+<script>
+import VDrawer from '@/components/004-Drawer/index'
+export default {
+  components: {
+    VDrawer
+  },
+  props: {},
+  data() {
+    return {
+      display: false,
+      drawerWidth: '500px'
+    }
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filter() {},
+  computed: {},
+  watch: {}
+}
+</script>
+<style scoped>
+</style>
