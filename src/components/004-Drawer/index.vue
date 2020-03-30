6e7e2361be2bcd986805c8a7bde5c55327c641dd
wangshuisheng <602466011@qq.com>
2020-03-30 21:56:07 +0800

    添加:Drawder组件

---
+++ src/components/004-Drawer/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="drawer">
+    <div class="mask"></div>
+    <div class="content">好好学习</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filter() {},
+  computed: {},
+  watch: {}
+}
+</script>
+<style scoped lang="scss">
+.mask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(134, 124, 124, 0.5);
+  z-index: 999;
+}
+</style>
