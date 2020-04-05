45523fbff6fb50f7c1a7b504789c46186b3e0714
wangshuisheng <602466011@qq.com>
2020-04-05 19:13:02 +0800

    更新:Drawer组件

--- src/components/004-Drawer/index.vue
+++ src/components/004-Drawer/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="drawer" :class="{'open':visible}">
     <div class="mask" @click="hideDrawer"></div>
-    <div class="content"></div>
+    <div class="content" :class="direction">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -12,6 +14,10 @@ export default {
     visible: {
       type: Boolean,
       default: false
+    },
+    direction: {
+      type: String,
+      default: 'right'
     }
   },
   data() {
@@ -25,7 +31,7 @@ export default {
   destroyed() {},
   methods: {
     hideDrawer() {
-      this.$emit('hide',false)
+      this.$emit('hide', false)
     }
   },
   filter: {},
@@ -41,17 +47,51 @@ export default {
       display: block;
     }
     .content {
-      top: 0;
+      &.right {
+        right: 0;    
+      }
+      &.left {
+        left: 0;
+      }
+      &.top {
+        top: 0;
+      }
+      &.bottom {
+        bottom: 0;
+      }
     }
   }
   .content {
     position: fixed;
-    width: 100%;
-    height: 30%;
-    top: -30%;
-    left: 0;
     background-color: pink;
-    transition: all 0.5s ease;
+    // 这里不能使用过度效果,横向和纵向进行切换时,就会有问题了
+    // transition: all 0.5s ease;  
+    &.right {
+      width: 30%;
+      height: 100%;
+      top: 0;
+      bottom: 0;
+      right: -30%;
+    }
+    &.left {
+      width: 30%;
+      height: 100%;
+      top: 0;
+      bottom: 0;
+      left: -30%;
+    }
+    &.top {
+      width: 100%;
+      height: 30%;
+      left: 0;
+      top: -30%;
+    }
+    &.bottom{
+      width: 100%;
+      height: 30%;
+      left: 0;
+      bottom:-30%;
+    }
   }
   .mask {
     position: fixed;
