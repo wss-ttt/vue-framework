5b0d6f696dc4d50693e6bd50303059f65e8df509
wangshuisheng <602466011@qq.com>
2020-04-05 19:13:27 +0800

    添加:test.vue

---
+++ src/views/000-test.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="test">
+    <button @click="drawer = true">打开</button>
+    <el-drawer :visible.sync="drawer" :modal="true"></el-drawer>
+    <!-- 模仿element-ui的结构 -->
+    <!-- <div class="drawer-wrapper">
+        <div class="drawer-container">
+            <div class="drawer"></div>
+        </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      drawer: false
+    }
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filter: {},
+  computed: {},
+  watch: {}
+}
+</script>
+<style scoped>
+
+</style>
