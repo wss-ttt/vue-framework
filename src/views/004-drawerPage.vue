2df68ec5ab8ae2bbf91e8feabafa69f06f61d02c
wangshuisheng <602466011@qq.com>
2020-04-05 13:30:16 +0800

    更新:drawerePage

--- src/views/004-drawerPage.vue
+++ src/views/004-drawerPage.vue
@@ -1,38 +1,7 @@
 <template>
   <div class="wrapper">
     <button type="button" @click="visible = true">打开</button>
-    <!-- <v-drawer title="测试" :visible.sync="visible" width="500px" close-on-click-modal></v-drawer>例子（完整属性） -->
-    <v-drawer
-      :visible.sync="visible"
-      :headerShow="true"
-      header-background="#f5f5f5"
-      title-color="#000"
-      main-background="#EBEEF5"
-      :footerShow="true"
-      footer-height="60px"
-      footer-background="#f5f5f5"
-      width="50%"
-      height="300px"
-      align="right"
-      :modal="true"
-      close-on-click-modal
-      :loading.sync="loading"
-      loadingColor="#ff6700"
-      @close="ce"
-      @closed="ce"
-      @open="ce"
-      @opend="ce"
-    >
-      <div slot="header">
-        <p>内容</p>
-      </div>
-      <div>
-        <p>内容</p>
-      </div>
-      <div slot="footer">
-        <p>底部</p>
-      </div>
-    </v-drawer>
+    <v-drawer :visible="visible" @hide="hide"></v-drawer>
   </div>
 </template>
 
@@ -54,8 +23,12 @@ export default {
   deactivated() {},
   updated() {},
   destroyed() {},
-  methods: {},
-  filter() {},
+  methods: {
+    hide(){
+      this.visible = false
+    }
+  },
+  filter: {},
   computed: {},
   watch: {}
 }
