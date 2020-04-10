8caa4e27b8c0e7576645a08b121e8524b670a046
USER-8LNJD3T32B\Administrator <602466011@qq.com>
2020-04-10 14:11:36 +0800

    更新:配置全局的sass变量文件

--- build/utils.js
+++ build/utils.js
@@ -60,7 +60,13 @@ exports.cssLoaders = function (options) {
     postcss: generateLoaders(),
     less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    // 修改成如下这样子
+    scss: generateLoaders('sass').concat({
+      loader:'sass-resources-loader',
+      options:{
+        resources:path.resolve(__dirname,'./../src/assets/scss/variables.scss')
+      }
+    }),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
@@ -99,3 +105,4 @@ exports.createNotifierCallback = () => {
     })
   }
 }
+
